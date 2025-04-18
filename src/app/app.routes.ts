a5536fcf7590e4a77e5e1518d20af63552bcd526
nRicca <nricca@wheretech.it>
2025-04-18 18:07:20 +0200

    feat (guard) : -- added guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,24 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
-import { MapEsriComponent } from './components/map-esri/map-esri.component';
 import { FindPlaceComponent } from './components/find-place/find-place.component';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './guard/auth/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'home',
     component: LayoutComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'find-place',
     component: FindPlaceComponent,
   },
-  {path : '**', component : LoginComponent}
+  { path: '**', component: LoginComponent },
 ];
